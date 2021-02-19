5f7ce243f64cde31632ea1a3ef734471ed504315
ArielMejiaDev <arielmejiadev@gmail.com>
2021-02-18 20:46:08 -0600

    wip

---
+++ resources/views/components/landing/footer.blade.php
@@ -0,0 +1,31 @@
+<footer class="flex justify-center px-4 text-white bg-gray-800">
+    <div class="container py-6">
+        <h1 class="text-lg font-bold text-center lg:text-2xl">
+            Join Thousands of Artisans developers and never miss <br> out on new tips, tutorials, and more.
+        </h1>
+
+        <div class="flex justify-center mt-6">
+            <div class="relative text-gray-700 w-full md:w-1/3">
+                <input class="w-full h-10 pl-3 pr-8 text-base text-gray-600 placeholder-gray-400 border rounded-lg focus:shadow-outline" type="text" placeholder="Enter your email..."/>
+                <button class="absolute inset-y-0 right-0 flex items-center px-4 font-bold text-white bg-indigo-600 rounded-r-lg hover:bg-indigo-500 focus:bg-indigo-700 focus:outline-none">Subscribe</button>
+            </div>
+        </div>
+
+        <hr class="h-px mt-6 border-gray-300 border-none dark:bg-gray-700">
+
+        <div class="flex flex-col items-center justify-between mt-6 md:flex-row">
+            <div>
+                <a href="#" class="text-xl font-bold text-white hover:text-gray-300">Snippet Collector</a>
+            </div>
+
+            <div class="flex mt-4 md:m-0">
+                <div class="-mx-4">
+                    <a href="#" class="px-4 text-sm font-medium text-gray-200 hover:text-gray-400 hover:underline">About</a>
+                    <a href="#" class="px-4 text-sm font-medium text-gray-200 hover:text-gray-400 hover:underline">Blog</a>
+                    <a href="#" class="px-4 text-sm font-medium text-gray-200 hover:text-gray-400 hover:underline">News</a>
+                    <a href="#" class="px-4 text-sm font-medium text-gray-200 hover:text-gray-400 hover:underline">Contact</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/components/landing/hero.blade.php
@@ -0,0 +1,47 @@
+<header class="bg-gray-900 pattern">
+    <div class="container px-6 mx-auto sm:py-32">
+        <nav class="flex flex-col py-2 sm:flex-row sm:justify-between sm:items-center lg:pt-10">
+            <div>
+                <a href="#" class="text-2xl font-semibold text-white hover:text-gray-300">Snippet Collector</a>
+            </div>
+
+            <div class="flex items-center mt-2 -mx-2 sm:mt-0">
+                <a href="#" class="px-3 py-1 text-sm font-semibold text-white transition-colors duration-200 transform border-2 rounded hover:bg-gray-700">Sign In</a>
+                <a href="#" class="px-3 py-2 mx-2 text-sm font-semibold text-white transition-colors duration-200 transform bg-black rounded hover:bg-gray-800">Sign Up</a>
+            </div>
+        </nav>
+
+        <div class="flex flex-col items-center py-6 md:h-128 md:flex-row lg:py-20 md:mr-2">
+            <div class="md:w-1/2">
+                <h2 class="text-4xl font-semibold text-gray-100">Snippet Collector</h2>
+
+                <h3 class="text-2xl font-semibold text-gray-100">
+                    Hello <span class="text-indigo-400">Laravel Artisan</span>
+                </h3>
+
+                <p class="mt-3 text-gray-100">Platform to save & get review of code snippets for Laravel Community.</p>
+            </div>
+
+            <div class="flex mt-8 w-full md:w-1/2 md:justify-end md:mt-0">
+                <div class="max-w-md bg-white rounded-lg dark:bg-gray-800 w-full">
+                    <div class="p-5 text-center">
+                        <h2 class="text-2xl font-semibold text-gray-700 dark:text-white fo">Sign In</h2>
+
+                        <form action="#">
+                            <div class="mt-4">
+                                <input class="block w-full px-4 py-2 text-gray-700 placeholder-gray-500 bg-white border border-gray-300 rounded dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" type="email" placeholder="Email address" aria-label="Email address">
+                                <input class="block w-full px-4 py-2 mt-4 text-gray-700 placeholder-gray-500 bg-white border border-gray-300 rounded dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring" type="password" placeholder="Password" aria-label="Password">
+                            </div>
+
+                            <div class="flex items-center justify-between mt-4">
+                                <a href="#" class="text-sm text-gray-600 dark:text-gray-200 hover:underline">Forget Password?</a>
+
+                                <button class="px-4 py-2 font-semibold text-white transition-colors duration-200 transform bg-gray-900 rounded hover:bg-gray-800 dark:hover:bg-gray-700 focus:outline-none focus:bg-gray-800 dark:focus:bg-gray-700">Login</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
